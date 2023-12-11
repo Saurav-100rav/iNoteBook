4a347cc0b2ebcc157dc7d341274ef6987f7ef018
Saurav-100rav <sk9234675221@gmail.com>
2023-12-11 10:32:21 +0530

    temporary.

--- client/src/components/home/Homepage.jsx
+++ client/src/components/home/Homepage.jsx
@@ -1,12 +1,29 @@
 import React from 'react'
-import {useContext} from 'react'
+import {useContext,useEffect} from 'react'
 import { NoteContext } from '../../context/NoteContext'
+import axios from 'axios'
 const Homepage = () => {
     const user = useContext(NoteContext);
-    setTimeout(()=>{
-        alert("name changed...")
-        user.setUser("100rav");
-    },5000)
+    // setTimeout(()=>{
+    //     alert("name changed...")
+    //     user.setUser("100rav");
+    // },5000)
+    useEffect(() => {
+      //   if (isAuthenticated === false) {
+      //       console.log(isAuthenticated);
+      //     navigate("/login");
+      //   }
+      // }, [isAuthenticated]);
+        getTempData();
+      },[])
+      const getTempData = async()=>{
+        try {
+          const res = await axios.get("https://inotebook-backend-xi93.onrender.com/api/v1/notes/getallTasks",{ withCredentials: true });
+          console.log("All Notes = ",res);
+        } catch (error) {
+          console.log(error);
+        }
+      }
   return (
     <div>
         Homepage : {user.user}
