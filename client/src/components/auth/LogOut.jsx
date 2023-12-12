dfed4f6f9dde04253c95b659ca4d8b7845d367da
Saurav-100rav <sk9234675221@gmail.com>
2023-12-12 18:22:06 +0530

    logic of remove token while logout

--- client/src/components/auth/LogOut.jsx
+++ client/src/components/auth/LogOut.jsx
@@ -1,14 +1,20 @@
 import React, { useEffect } from 'react'
-import {useDispatch, useSelector} from 'react-redux'
-import { logout } from '../../store/userSlice';
+// import {useDispatch, useSelector} from 'react-redux'
+// import { logout } from '../../store/userSlice';
 import { useNavigate } from 'react-router-dom';
 const LogOut = () => {
-    const dispatch = useDispatch();
-    const {user,isAuthenticated,status} = useSelector((state)=>state.User);
+    // const dispatch = useDispatch();
+    // const {user,isAuthenticated,status} = useSelector((state)=>state.User);
     const navigate = useNavigate();
     useEffect(()=>{
-        dispatch(logout());
+        // dispatch(logout());
+        removeUser();
     },[])
+    const removeUser = ()=>{
+      localStorage.clear();
+      alert("LogOut successfully...");
+      navigate("/login");
+    }
   return (
     <div>LogOut</div>
   )
